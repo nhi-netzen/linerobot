25f380ece102c872bcc762552a3d4f60f81cf20a
nhi-netzen <dtynhi1709@gmail.com>
2025-11-03 21:27:33 +0700

    config.h
    Removed unused variables and commented-out code related to sensors and motor speeds.

--- config.h
+++ config.h
@@ -10,7 +10,7 @@
 #define BIN2 6
 #define PWMB 5
 
-// Varibles to switch the direction of the motors
+// Direction switch
 const int offsetA = 1;
 const int offsetB = 1;
 
@@ -18,28 +18,7 @@ const int offsetB = 1;
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
-// Set up the (fake) qtr sensor
-QTRSensors qtr;
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
-// ====== PID Constants ======
-extern float Kp;
-extern float Ki;
-extern float Kd;
+// Set the pins for the sensor array
+const int sensorPins[5] = {2, 3, 4, 12, 13};
+int sensorState[5];
 
-// Timer for line loss detection
-unsigned long lineDetectedTime = 0;
-const unsigned long LINE_LOST_TIMEOUT = 777; // threshold (ms)
-
-// Motor speeds
-const int MIN_SPEED = 20;
-const int MAX_SPEED = 255;
-int baseSpeed = 140;
-int leftSpeed = 0;
-int rightSpeed = 0;
-
-// Threshold for the robot to stop and correct itself
-const int CORRECTION_THRESHOLD = 2000;
-const int ROTATION_SPEED = 100;
-
-#endif
